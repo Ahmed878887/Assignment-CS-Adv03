a32ccf81ad15088ab7d95a8070d53a87d6676fc9
Dator <Dator@DATOR>
2025-08-09 22:31:34 +0200

    Add project files.

---
+++ Assignment-CS-Adv03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_CS_Adv03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
